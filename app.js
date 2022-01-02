83ebf904e13516d21bac18b43e5c43e267fc5bb9
ToànErrors <72167690+Toan-Errors@users.noreply.github.com>
2022-01-02 16:05:12 +0700

    123

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 
+
 app.use(express.json());
 
-const PORT = process.env.NODEJS_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use('/items', require('./routes/itemRouter'));
 
--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+
 const host = process.env.DB_HOST || '127.0.0.1';
 const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_NAME || 'todoapp';
